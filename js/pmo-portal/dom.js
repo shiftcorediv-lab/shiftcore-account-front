a468863e07e0a1611b79896757199cb89a369717
shiftcorediv-lab <shiftcore.div@gmail.com>
2026-04-28 19:11:03 +0900

    Update dom.js

--- js/pmo-portal/dom.js
+++ js/pmo-portal/dom.js
@@ -1,1 +1,10 @@
+export const userNameBox = document.getElementById("userNameBox");
+export const employeeCodeBox = document.getElementById("employeeCodeBox");
+export const accountMetaArea = document.getElementById("accountMetaArea");
+export const entryBannerArea = document.getElementById("entryBannerArea");
 
+export const goApplyBtn = document.getElementById("goApplyBtn");
+export const goManageBtn = document.getElementById("goManageBtn");
+export const backToDashboardBtn = document.getElementById("backToDashboardBtn");
+export const manageHint = document.getElementById("manageHint");
+export const messageBox = document.getElementById("messageBox");

4b03151656525ac8dc5486cd8f442470818c586f
shiftcorediv-lab <shiftcore.div@gmail.com>
2026-05-04 16:44:36 +0900

    Create dom.js

---
+++ js/pmo-admin/dom.js
@@ -0,0 +1,15 @@
+export const userNameBox = document.getElementById("userNameBox");
+export const employeeCodeBox = document.getElementById("employeeCodeBox");
+export const accountMetaArea = document.getElementById("accountMetaArea");
+export const entryBannerArea = document.getElementById("entryBannerArea");
+
+export const monthSelect = document.getElementById("monthSelect");
+export const monthHint = document.getElementById("monthHint");
+export const manageHint = document.getElementById("manageHint");
+
+export const openMonthlyBtn = document.getElementById("openMonthlyBtn");
+export const openRequestBtn = document.getElementById("openRequestBtn");
+export const downloadCsvBtn = document.getElementById("downloadCsvBtn");
+export const backToDashboardBtn = document.getElementById("backToDashboardBtn");
+
+export const messageBox = document.getElementById("messageBox");

817762290f6591fbe38aacd199b15d32eb041a97
shiftcorediv-lab <shiftcore.div@gmail.com>
2026-05-06 19:54:11 +0900

    Update config.js
    最新デプロイURLじゃなかったのを修正

--- js/signup-admin/config.js
+++ js/signup-admin/config.js
@@ -1,3 +1,3 @@
-export const SIGNUP_ADMIN_API_URL = "https://script.google.com/macros/s/AKfycbyTQlhU9osbUqh0cjJJCzERgs8fuQ1gr3x5svc-aw3TziPbhCE1UWQTCfgsjARKl7FiJw/exec";
+export const SIGNUP_ADMIN_API_URL = "https://script.google.com/macros/s/AKfycbxoWeQnL4cuF-hYVMuLpYDMJ8e1ip_x1WpQfAW4HInzbX_RGYAZ5L7iiS5RDEm6TG8shA/exec";
 export const ACCOUNT_PORTAL_URL = "./account-portal.html";
 export const SIGNUP_ADMIN_ALLOWED_ROLES = ["admin", "developer"];

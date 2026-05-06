6358cc01930b1e1fd9d939239c6c282b50f33172
shiftcorediv-lab <shiftcore.div@gmail.com>
2026-05-06 19:36:39 +0900

    Create config.js

---
+++ js/signup-admin/config.js
@@ -0,0 +1,3 @@
+export const SIGNUP_ADMIN_API_URL = "https://script.google.com/macros/s/AKfycbyTQlhU9osbUqh0cjJJCzERgs8fuQ1gr3x5svc-aw3TziPbhCE1UWQTCfgsjARKl7FiJw/exec";
+export const ACCOUNT_PORTAL_URL = "./account-portal.html";
+export const SIGNUP_ADMIN_ALLOWED_ROLES = ["admin", "developer"];

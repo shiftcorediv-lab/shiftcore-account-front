29b36b9d07d817951e17520bad1237a9832a6bc3
shiftcorediv-lab <shiftcore.div@gmail.com>
2026-05-18 00:09:32 +0900

    Update api.js

--- js/login/api.js
+++ js/login/api.js
@@ -15,3 +15,19 @@ export async function checkUserWithGas(email) {
   const result = await response.json();
   return result;
 }
+
+export async function resolveCurrentUserWithGasByIdToken(idToken) {
+  const response = await fetch(LOGIN_CHECK_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "text/plain;charset=utf-8"
+    },
+    body: JSON.stringify({
+      action: "resolveCurrentUserByIdToken",
+      idToken: idToken
+    })
+  });
+
+  const result = await response.json();
+  return result;
+}
